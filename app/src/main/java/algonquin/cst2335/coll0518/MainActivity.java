b22e02c4645938522c35a1993d9df24602eb41ab
Camryn Collis <coll0518@algonquinlive.com>
2023-05-11 17:57:07 -0400

    initial commit

---
+++ app/src/main/java/algonquin/cst2335/coll0518/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.coll0518;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
